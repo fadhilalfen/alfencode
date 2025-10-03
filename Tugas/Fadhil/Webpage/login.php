6701a4d8c957e527c34f0e5192dd877a11c5ac51
Muhammad Fadhil Sulaiman <55867031+fadhilalfen@users.noreply.github.com>
2025-10-03 16:17:05 +0700

    Delete Tugas/Fadhil directory

--- Tugas/Fadhil/Webpage/login.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="login.css">
-<div class="pengumuman"><h1>Login HTML Fadhil</h1>
-<form method = "POST" action = "form.php">
-Nama: <input type = "text" name="nama" /><br/><br/>
-<input type = "submit" value="Kirim"/>
-</div>
